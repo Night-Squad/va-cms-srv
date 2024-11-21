6e03ae92354730fa9d2f129d3c614f9085d188b4
wiyanayusuf <wiyana.yusuf@gmail.com>
2024-11-21 21:26:25 +0700

    Fixing master corporate update api

--- src/main/java/com/va/corporate/srv/repository/vav2/MasterCorporateRepository.java
+++ src/main/java/com/va/corporate/srv/repository/vav2/MasterCorporateRepository.java
@@ -25,7 +25,7 @@ public class MasterCorporateRepository {
     private static final String FINDALL = "SELECT * FROM master_corporation WHERE 1=1";
 
     private static final String COUNTALL = "SELECT COUNT(*) FROM master_corporation WHERE 1=1";
-    private static final String UPDATE = "UPDATE master_corporation SET corporate_name = ? WHERE id = ?";
+    private static final String UPDATE = "UPDATE master_corporation SET corporate_name = ?, is_active = ?, updated_at = ?, updated_by = ? WHERE id = ?";
     private static final String UPDATE_IS_ACTIVE = "UPDATE master_corporation SET is_active = false WHERE id = ?";
 
     private GeneralHandlerQuery generalHandlerQuery;
@@ -103,7 +103,12 @@ public class MasterCorporateRepository {
     }
 
     public void updateMasterCorporation(MasterCorporateModel masterCorporation){
-        jdbcTemplate.update(UPDATE, masterCorporation.getCorporateName(), masterCorporation.getId());
+        jdbcTemplate.update(UPDATE,
+                masterCorporation.getCorporateName(),
+                masterCorporation.getIsActive(),
+                masterCorporation.getUpdatedAt(),
+                masterCorporation.getUpdatedBy(),
+                masterCorporation.getId());
     }
 
     public void deleteMasterCorporation(Long id){
